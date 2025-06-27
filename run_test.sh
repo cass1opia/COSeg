5558ed07811f11105c75ae1a999bc8bf65c7e61f
as65xici <adrian_schmidt@outlook.de>
2025-06-27 12:12:47 +0300

    fixed path

--- run_test.sh
+++ run_test.sh
@@ -3,4 +3,4 @@ python3 scripts/test_multipoint_loader.py \
     --config config/essen_outdoor_COSeg_fs.yaml \
     --epochs 3 \
     --episodes 2 \
-    --save_dir datasets/test
+    --save_dir /sc/projects/sci-doellner/chair/adrian.schmidt/coseg_data/essen-road/epochs
