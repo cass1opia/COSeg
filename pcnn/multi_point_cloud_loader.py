def7f1643bb20ef05be77459b31134acfebf0f5d
as65xici <adrian_schmidt@outlook.de>
2025-06-27 11:54:48 +0300

    test

--- pcnn/multi_point_cloud_loader.py
+++ pcnn/multi_point_cloud_loader.py
@@ -862,8 +862,8 @@ class MultiPointCloudLoaderFS:
 
             if neighborhoods:
                 neighborhood = neighborhoods[0]
-                print("neighborhood type:", type(neighborhood))
-                print("neighborhood:", neighborhood)
+
+
                 # Extract coordinates and features
                 if isinstance(neighborhood, dict):
                     # Get the points array which contains [x, y, z, r, g, b, class, intensity]
