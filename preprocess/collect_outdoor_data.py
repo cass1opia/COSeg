9ebcc083352bd00d4457ffcce521a92df4cf93b8
as65xici <adrian_schmidt@outlook.de>
2025-07-31 11:48:14 +0200

    fix

--- preprocess/collect_outdoor_data.py
+++ preprocess/collect_outdoor_data.py
@@ -1,10 +1,12 @@
 import os
 import numpy as np
 
+
 def map_street_sign_class(data):
     data[:, 9] = np.where(data[:, 8] == 15, data[:, 9], 0)
     return data
-    
+
+
 if __name__ == "__main__":
     import argparse
 
@@ -27,8 +29,10 @@ if __name__ == "__main__":
     if not os.path.exists(SAVE_PATH):
         os.makedirs(SAVE_PATH)
 
-    txt_files = [f for f in os.listdir(DATA_PATH) if f.endswith('.txt')]
+    txt_files = [f for f in os.listdir(DATA_PATH) if f.endswith(".txt")]
     for txt_file in txt_files:
-        data = np.loadtxt(os.path.join(DATA_PATH, txt_file), delimiter=' ', skiprows=1)
+        data = np.loadtxt(os.path.join(DATA_PATH, txt_file), delimiter=" ", skiprows=1)
         data = map_street_sign_class(data)
-        np.save(os.path.join(SAVE_PATH, txt_file.replace('.txt', '_processed.npy')), data)
+        np.save(
+            os.path.join(SAVE_PATH, txt_file.replace(".txt", "_processed.npy")), data
+        )
