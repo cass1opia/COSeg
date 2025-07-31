1d73a56a6f38ff41819d808c889f1da6ccd2c4f5
as65xici <adrian_schmidt@outlook.de>
2025-07-31 12:48:54 +0200

    fix ?

--- preprocess/collect_outdoor_data.py
+++ preprocess/collect_outdoor_data.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from util.logger import get_logger
 
 def map_street_sign_class(data):
-    data[:, 6] = np.where(data[:, 7] == 15, data[:, 7], data[:, 6] + 13)
+    data[:, 6] = data[:, 6] + 13
     data = np.delete(data, 7, axis=1)
     return data
     
