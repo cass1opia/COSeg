b31643f041c52b9c1da90237bd5785e3a5fab3de
as65xici <adrian_schmidt@outlook.de>
2025-08-12 08:59:40 +0200

    test

--- scripts/generate_pointcloud_stats.py
+++ scripts/generate_pointcloud_stats.py
@@ -45,6 +45,8 @@ def analyze_pointcloud(file_path):
         xyz_min = np.amin(xyz, axis=0)
         xyz -= xyz_min
         xyz_max = np.amax(xyz, axis=0)
+
+        print(np.amin(xyz, axis=0))
         # Labels aus der 7. Spalte (Index 6) extrahieren
         labels = pointcloud[:, 6]
         
