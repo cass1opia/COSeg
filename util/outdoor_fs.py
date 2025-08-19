e86f460bb5229d89ea6044fcdf547a0a1e08d714
as65xici <adrian_schmidt@outlook.de>
2025-08-19 11:04:20 +0200

    fix

--- util/outdoor_fs.py
+++ util/outdoor_fs.py
@@ -725,11 +725,6 @@ class Outdoor_FS_TEST(Dataset):
                     self.file_names.append(out_filename)
                     episode_ind += 1
 
-
-            with open(os.path.join(os.path.join(self.data_root, "stats"), "class_combination_2_episodes.csv"), "w") as f:
-                f.write("class_combination,num_episodes\n")
-                for class_combination in class_comb:
-                    f.write(f"{class_combination},{self.num_episode_per_comb}\n")
             
 
     def __len__(self):
